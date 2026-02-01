ed852649b1d9557dbfb6f08558c8c6bd90e98322
MonicaFernandezM <monicamfmiras@gmail.com>
2026-02-01 20:22:55 +0100

    Solved lab

---
+++ temporary_tables.sql
@@ -0,0 +1,56 @@
+USE sakila;
+
+-- 1. Create a View
+-- First, create a view that summarizes rental information for each customer. 
+-- The view should include the customer's ID, name, email address, and total number of rentals 
+-- (rental_count).
+
+CREATE OR REPLACE VIEW customer_rental_summary AS 
+SELECT
+c.customer_id,
+CONCAT(c.first_name, ' ', c.last_name) AS customer_name, -- apendiendo que esto se puede hacer
+c.email,
+COUNT(r.rental_id) AS rental_count
+FROM customer c
+LEFT JOIN rental AS r
+ON c.customer_id = r.customer_id
+GROUP BY 
+c.customer_id, c.first_name, c.last_name, c.email;
+
+-- 2. Create a Temporary Table
+-- Next, create a Temporary Table that calculates the total amount paid by each customer 
+-- (total_paid). The Temporary Table should use the rental summary view created in Step 1 to 
+-- join with the payment table and calculate the total amount paid by each customer.
+
+CREATE TEMPORARY TABLE customer_payment_summary AS 
+SELECT 
+crs.customer_id, 
+SUM(p.amount) AS total_paid
+FROM customer_rental_summary AS crs
+LEFT JOIN payment AS p 
+ON crs.customer_id = p.customer_id
+GROUP BY 
+crs.customer_id;
+
+-- 3. Create a CTE and the Customer Summary Report
+-- Create a CTE that joins the rental summary View with the customer payment summary Temporary 
+-- Table created in Step 2. The CTE should include the customer's 
+-- name, email address, rental count, and total amount paid.
+
+WITH customer_summary AS (
+    SELECT
+	crs.customer_name,
+	crs.email,
+	crs.rental_count,
+	cps.total_paid
+    FROM customer_rental_summary crs
+    JOIN customer_payment_summary cps
+	ON crs.customer_id = cps.customer_id
+)SELECT
+customer_name,
+email,
+rental_count,
+total_paid,
+ROUND(total_paid / rental_count, 2) AS avg_payment_per_rental
+FROM customer_summary;
+
